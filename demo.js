e48adaedea6b0a9825324cbe2a367fa32c5f8f55
jerry120202 <76770872+jerry120202@users.noreply.github.com>
2025-01-30 00:10:32 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,26 @@
+document.write("Hello JavaScript by JavaScript \n");
+
+// alert("Hello Javatpoint"); 
+
+function msg()
+{
+	// alert("Hello Javatpoint");
+	document.write("Hello JavaScript by JavaScript \n");
+}
+
+// var a=10;  
+// var b=20;  
+// var c=a+b;
+// document.writeln(c + "\n");
+
+var data=200;//gloabal variable  
+function a()
+{  
+	document.write(data);  
+}  
+function b()
+{  
+	document.write(data);  
+}  
+// a();
+// b(); 
